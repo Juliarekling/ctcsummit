ccb2241bfafbe37c7e85a76c0d7c668ed7340c9a
Juliarekling <juliarekling@gmail.com>
2025-08-04 17:47:51 +0400

    app.py

--- app.py
+++ app.py
@@ -1,15 +1,6 @@
 from flask import Flask, render_template
-import sqlite3
 
 app = Flask(__name__)
-
-def get_data(table):
-    conn = sqlite3.connect('data.db')
-    cursor = conn.cursor()
-    cursor.execute(f"SELECT * FROM {table}")
-    rows = cursor.fetchall()
-    conn.close()
-    return rows
 
 @app.route("/")
 def index():
@@ -17,13 +8,15 @@ def index():
 
 @app.route("/speakers")
 def speakers():
-    speakers_data = get_data('speakers')
-    return render_template("speakers.html", speakers=speakers_data)
+    return render_template("speakers.html")
 
 @app.route("/partners")
 def partners():
-    partners_data = get_data('partners')
-    return render_template("partners.html", partners=partners_data)
+    return render_template("partners.html")
+
+@app.route("/contacts")
+def contacts():
+    return render_template("contacts.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
