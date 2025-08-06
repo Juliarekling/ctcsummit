9ad51eb161c95bcaa8fbc52e98c6e03dafe94b9f
Juliarekling <juliarekling@gmail.com>
2025-08-06 16:58:37 +0400

    app.py

--- app.py
+++ app.py
@@ -2,21 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route("/speakers")
+@app.route('/speakers')
 def speakers():
-    return render_template("speakers.html")
+    return render_template('speakers.html')
 
-@app.route("/partners")
+@app.route('/partners')
 def partners():
-    return render_template("partners.html")
+    return render_template('partners.html')
 
-@app.route("/contacts")
+@app.route('/faq')
+def faq():
+    return render_template('FAQ.html')
+
+@app.route('/contacts')
 def contacts():
-    return render_template("contacts.html")
+    return render_template('contacts.html')
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
